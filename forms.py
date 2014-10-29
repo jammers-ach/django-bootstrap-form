c889c0edacf6eaf09b431130ba3e06c6c37c482f
jammers <jammers@gmail.com>
2014-10-29 18:13:17 +0300

    bootstrap messages added, forms updated

--- forms.py
+++ forms.py
@@ -1,13 +1,21 @@
-
+from templatetags.bootstrap import bootstrap_horizontal
 from django import forms
 
 class BootstrapModelForm(forms.ModelForm):
-    def as_p(self):
-        return "James is super cool - MODEL FORM"
+    '''A model form that will render itself in bootstrap'''
+    required_css_class = 'required'
+
+    def as_bootstrap(self):
+        return bootstrap_horizontal(self)
+
+    class Media:
+        css = {
+            'all': ('bootstrap_form/css/forms.css',)
+        }
+
 
 class BootstrapForm(forms.Form):
     '''A form subclass that can be rendered as bootstrap form'''
 
-
     def as_p(self):
-        return "James is super cool"
+        return "James is super cool - Ordinary form"
--- templatetags/bootstrap.py
+++ templatetags/bootstrap.py
@@ -3,7 +3,7 @@ from django.template import Context
 from django.template.loader import get_template
 from django import template
 
-from bootstrapform import config
+from .. import config
 
 register = template.Library()
 
