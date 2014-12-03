bbc153ea1ff67a9714f5b54daee3e7c7ca0162ed
jammers <jammers@gmail.com>
2014-12-03 14:54:49 +0300

    ChoicedOtherField

---
+++ fields.py
@@ -0,0 +1,48 @@
+'''
+Fields.py - new fields for models
+'''
+
+from django.db.models import Field,SubfieldBase
+from .widgets import BootstrapChoiceOtherField
+#import os
+#from django.conf import settings
+
+class ChoicedOtherField(Field):
+    '''a field that is restricted to the model+field in question'''
+
+    description = 'A field that is restricted to the model in question'
+
+    __metaclass__ = SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        #self.widget = MultipleFileWidget()
+        self.obj_mod = kwargs.pop('obj_mod')
+        self.obj_classname = kwargs.pop('obj')
+        field = kwargs.pop('field')
+        if(isinstance(field,str)):
+            self.fields = [field]
+        else:
+            self.fields = field
+
+        self.default_choices  = kwargs.pop('default_choices')
+
+        super(ChoicedOtherField,self).__init__(*args,**kwargs)
+
+
+    def formfield(self, **kwargs):
+        #defaults = {'widget': self.widget}
+        mod = __import__(self.obj_mod)
+        klass = getattr(mod.models, self.obj_classname)
+        required = not self.blank
+
+
+        defaults = {'widget':BootstrapChoiceOtherField(klass=klass,fields=self.fields,default_choices=self.default_choices,required=required),
+                    }
+        defaults.update(kwargs)
+        return super(ChoicedOtherField, self).formfield(**defaults)
+
+    def get_internal_type(self):
+        return "TextField"
+
+
+
